989dd1fb43bcb9a37bf6d13effb340a7e88a5e39
Your Name <you@example.com>
2021-06-03 12:38:02 -0700

    Removed the unneeded json() in THEN.

--- main.js
+++ main.js
@@ -2,10 +2,11 @@ let api = 'https://catfact.ninja/breeds';
 
 let fetchAPI = async (api) => {
     let response = await fetch(api);
+    console.log('response: ', response);
     let json = response.json();
     return json;
 }
 
 fetchAPI(api)
-.then((response) => {console.log(response.json())})
+.then((response) => {console.log(response.data)})
 .catch((error) => console.log(error));
