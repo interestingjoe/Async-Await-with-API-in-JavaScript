2a6c7cde8eeb0eb69b9b1534cee62b3b1ae631e0
Your Name <you@example.com>
2021-06-03 12:35:10 -0700

    Fetching API.

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log(123);
+let api = 'https://catfact.ninja/breeds';
+
+let fetchAPI = async (api) => {
+    let response = await fetch(api);
+    let json = response.json();
+    return json;
+}
+
+fetchAPI(api)
+.then((response) => {console.log(response.json())})
+.catch((error) => console.log(error));
